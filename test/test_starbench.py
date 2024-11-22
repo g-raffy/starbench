2d7fc1a0aef4c2a83c9ee17a018fbbdb542f7363
Guillaume Raffy <guillaume.raffy@univ-rennes1.fr>
2024-11-22 10:33:52 +0100

    fixed unittest that was broken because it needed to be adapted to a change in starbench_cmake_app
    work related to [https://bugzilla.ipr.univ-rennes.fr/show_bug.cgi?id=3958]

--- test/test_starbench.py
+++ test/test_starbench.py
@@ -7,9 +7,6 @@ from starbench.existingdir import ExistingDir
 
 
 class StarbenchTestCase(unittest.TestCase):
-
-    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
-
     def setUp(self) -> None:  # pylint: disable=useless-parent-delegation
         return super().setUp()
 
@@ -18,9 +15,11 @@ class StarbenchTestCase(unittest.TestCase):
         source_code_provider = ExistingDir(Path('test/mamul1').absolute())
         tmp_dir = Path('tmp').absolute()
         benchmark_command = ['./mamul1', '3000', '10']
-        starbench_cmake_app(source_code_provider=source_code_provider, tmp_dir=tmp_dir, num_cores=2, benchmark_command=benchmark_command)
+        output_measurements_file_path = tmp_dir / 'measurements.tsv'
+        starbench_cmake_app(source_code_provider=source_code_provider, output_measurements_file_path=output_measurements_file_path, tmp_dir=tmp_dir, num_cores=2, benchmark_command=benchmark_command)
         # self.assertIsInstance(job_state, JobsState)
 
 
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
     unittest.main()
